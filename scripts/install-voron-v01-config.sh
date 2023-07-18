0738cb5daf459f2a0fca9a1365424a4eb1b2f03a
Oleg Dolya <oleg.dolya@gmail.com>
2023-07-18 14:32:24 +0300

    modify install voron v01

--- scripts/install-voron-v01-config.sh
+++ scripts/install-voron-v01-config.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-tail -n +2 /home/pi/printer_data/config/RatOS/templates/voron-v01-printer.template.cfg > /home/pi/printer_data/config/printer.cfg
+if [[ -e /dev/btt-octopus-11 ]]; then
+	echo "Installing Voron V0 btt-octopus-11 ..."
+	tail -n +2 /home/pi/printer_data/config/RatOS/templates/voron-v0.btt-octopus-11.printer.template.cfg > /home/pi/printer_data/config/printer.cfg
+	tail -n +2 /home/pi/printer_data/config/RatOS/templates/sensorless-homing-tmc2209.cfg > /home/pi/printer_data/config/sensorless-homing-tmc2209.cfg
+elif [[ -e /dev/btt-manta-m5p ]]; then
+	echo "[TODO] Installing Voron V0 /dev/btt-manta-m5p ..."
+	#tail -n +2 /home/pi/printer_data/config/RatOS/templates/voron-v0.btt-octopus-11.printer.template.cfg > /home/pi/printer_data/config/printer.cfg
+	#tail -n +2 /home/pi/printer_data/config/RatOS/templates/sensorless-homing-tmc2209.cfg > /home/pi/printer_data/config/sensorless-homing-tmc2209.cfg
+else
+	echo "Installing Voron V0 btt-skr-mini-e3-30 ..."
+	tail -n +2 /home/pi/printer_data/config/RatOS/templates/voron-v01-printer.template.cfg > /home/pi/printer_data/config/printer.cfg
+fi
